b000285310242507c2080559888d5b4885f9960b
Joel Blake <118257886+JTBlakey@users.noreply.github.com>
2022-11-26 15:54:51 +0000

    added all buttons

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,68 +28,272 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button19 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(272, 234);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // pictureBox2
             // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox2.Image = global::shop.Properties.Resources.logo;
-            this.pictureBox2.Location = new System.Drawing.Point(679, 0);
+            this.pictureBox2.Location = new System.Drawing.Point(689, 0);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(121, 126);
+            this.pictureBox2.Size = new System.Drawing.Size(111, 117);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 5;
             this.pictureBox2.TabStop = false;
             this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
             this.pictureBox2.DoubleClick += new System.EventHandler(this.pictureBox1_Click2);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(679, 132);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(121, 45);
-            this.button2.TabIndex = 6;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(679, 183);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(121, 51);
-            this.button3.TabIndex = 7;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
+            // button19
+            // 
+            this.button19.Location = new System.Drawing.Point(618, 289);
+            this.button19.Name = "button19";
+            this.button19.Size = new System.Drawing.Size(183, 175);
+            this.button19.TabIndex = 37;
+            this.button19.Text = "Subtotal";
+            this.button19.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(428, 289);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(194, 175);
+            this.button18.TabIndex = 36;
+            this.button18.Text = "Back";
+            this.button18.UseVisualStyleBackColor = true;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(214, 343);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(218, 121);
+            this.button17.TabIndex = 35;
+            this.button17.Text = "Rubber";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(214, 226);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(218, 119);
+            this.button16.TabIndex = 34;
+            this.button16.Text = "Pencil Sharpener";
+            this.button16.UseVisualStyleBackColor = true;
+            this.button16.Click += new System.EventHandler(this.button16_Click);
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(428, 111);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(194, 181);
+            this.button15.TabIndex = 33;
+            this.button15.Text = "Void";
+            this.button15.UseVisualStyleBackColor = true;
+            this.button15.Click += new System.EventHandler(this.button15_Click);
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(618, 111);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(183, 181);
+            this.button14.TabIndex = 32;
+            this.button14.Text = "Receipt";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(214, 111);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(218, 117);
+            this.button13.TabIndex = 31;
+            this.button13.Text = "Pencil";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(214, 0);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(218, 117);
+            this.button12.TabIndex = 30;
+            this.button12.Text = "Pencil Case";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(144, 406);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 58);
+            this.button11.TabIndex = 29;
+            this.button11.Text = "Red";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(73, 406);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 58);
+            this.button10.TabIndex = 28;
+            this.button10.Text = "Green";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(108, 289);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(111, 56);
+            this.button9.TabIndex = 27;
+            this.button9.Text = "30cm";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(108, 174);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(111, 54);
+            this.button8.TabIndex = 26;
+            this.button8.Text = "Blue";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 406);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 58);
+            this.button7.TabIndex = 25;
+            this.button7.Text = "Yellow";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(-1, 343);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(220, 66);
+            this.button6.TabIndex = 24;
+            this.button6.Text = "Highlighter";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(-1, 289);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 56);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "15cm";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(-1, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(219, 66);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Ruler";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 174);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 54);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Black";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(-1, 111);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(219, 66);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Pen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(-1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(219, 117);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Stock";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(428, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(264, 117);
+            this.groupBox1.TabIndex = 38;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "What you have selected:";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 463);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button19);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.button16);
+            this.Controls.Add(this.button15);
+            this.Controls.Add(this.button14);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
-            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
@@ -98,11 +302,27 @@
         }
 
         #endregion
-
-        private Button button1;
         private PictureBox pictureBox2;
-        private Button button2;
-        private Button button3;
         private OpenFileDialog openFileDialog1;
+        private Button button19;
+        private Button button18;
+        private Button button17;
+        private Button button16;
+        private Button button15;
+        private Button button14;
+        private Button button13;
+        private Button button12;
+        private Button button11;
+        private Button button10;
+        private Button button9;
+        private Button button8;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button1;
+        private GroupBox groupBox1;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -6,13 +6,6 @@ namespace shop
         {
             InitializeComponent();
         }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-            button.Text = "hello" + button1.Text;
-        }
-
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
@@ -26,6 +19,103 @@ namespace shop
         private void openFileDialog1_FileOk(object sender, System.ComponentModel.CancelEventArgs e)
         {
 
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "Please select a colour";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "PEN £0.10";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "PEN £0.10";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "Please select a length";
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "Ruler £0.35";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "Ruler £0.40";
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "Please select a colour";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "HIGHLIGHTER Y £0.45";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "HIGHLIGHTER G £0.45";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "HIGHLIGHTER R £0.45";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "PENCIL CASE £0.60";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "PENCIL £0.05";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "PENCIL SHARPENER £0.15";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = groupBox1.Text + "RUBBER £0.10";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            groupBox1.Text = "What you have selected:";
         }
     }
 }
